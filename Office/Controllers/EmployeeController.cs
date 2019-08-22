e3f9d2c06341708ce7e0e7d2f2c5a2413e58e519
aamirakhtar <aamiradvantage@gmail.com>
2019-08-22 23:25:47 +0500

    Add project files.

---
+++ Office/Controllers/EmployeeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Office.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            OfficeEntities oe = new OfficeEntities();
+            //ViewData["employees"] = oe.Employees.ToList();
+            ViewBag.employees = oe.Employees.ToList();
+            ViewBag.msg = TempData["msg"];
+
+            return View();
+        }
+
+        public ActionResult CreateEmployee()
+        {
+            OfficeEntities oe = new OfficeEntities();
+            ViewBag.departments = oe.Departments.ToList();
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateEmployee(FormCollection fc)
+        {
+            string name = fc["name"];
+            int departmentId = int.Parse(fc["departmentId"]);
+            int age = int.Parse(fc["age"]);
+            string designation = fc["designation"];
+            string address = fc["address"];
+            string gender = fc["gender"];
+
+            Employee emp = new Employee()
+            {
+                Name = name,
+                DepartmentId = departmentId,
+                Age = age,
+                Designation = designation,
+                Address = address,
+                Gender = gender
+            };
+            
+            OfficeEntities oe = new OfficeEntities();
+            oe.Employees.Add(emp);
+            oe.SaveChanges();
+
+            TempData["msg"] = "Successfully added";
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Office/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Office.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult HomeIndexSubmit()
+        {
+            string userName = Request.Form["userName"];
+            string password = Request.Form["password"];
+            int departmentType = int.Parse(Request.Form["departmentType"]);
+
+
+            //string connString = ConfigurationManager.ConnectionStrings["DefaultConn"].ToString();
+            //SqlConnection conn = new SqlConnection(connString);
+            //SqlCommand command = new SqlCommand("insert into department values(@Name,@Description)", conn);
+            //command.CommandType = System.Data.CommandType.StoredProcedure;
+
+            //command.Parameters.AddWithValue("@Name", userName);
+            //command.Parameters.AddWithValue("@Description", "hello");
+            //conn.Open();
+            //var reader = command.ExecuteNonQuery();
+            //conn.Close();
+
+            return RedirectToAction("Index");
+        }
+    }
+
+    public class DepartmentType
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
